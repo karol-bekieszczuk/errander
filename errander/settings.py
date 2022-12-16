22793f71e1b7481a472038dc805bfe433cfe1640
admin <test@test.com>
2022-12-16 17:50:29 +0100

    removed commented section about mysql from settings.py

--- errander/settings.py
+++ errander/settings.py
@@ -76,8 +76,6 @@ WSGI_APPLICATION = 'errander.wsgi.application'
 
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'errander',
         'USER': 'errander_user',
