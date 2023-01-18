02720caf2c607b5bb595eade7023b626fbf9b0a9
admin <test@test.com>
2023-01-18 22:42:17 +0100

    changed use of local settings to python-decouple

--- errander/settings.py
+++ errander/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
-from local.local_settings import *
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -33,7 +33,6 @@ INSTALLED_APPS = [
     'accounts',
     'errands',
     'simple_history',
-    # 'mapwidgets',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -110,9 +109,6 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, "static"),
-# ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
@@ -129,3 +125,22 @@ AUTH_USER_MODEL = 'accounts.User'
 
 LOGIN_URL = '/accounts/login_user'
 LOGOUT_URL = '/accounts/logout_user'
+
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+
+
+SECRET_KEY = config('SECRET_KEY')
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('PSQL_DB_NAME'),
+        'USER': config('PSQL_USER'),
+        'PASSWORD': config('PSQL_PASSWORD'),
+        'HOST': config('PSQL_HOST'),
+        'PORT': '',
+    }
+}
+
+GOOGLE_API_KEY = config('GOOGLE_API_KEY')
