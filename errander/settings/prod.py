891489213bfe6f9ff61ccbf02c33ac4af3b6e5c9
admin <test@test.com>
2023-02-05 16:29:15 +0100

    imported everything to prod.py from settings.ini

--- errander/settings/prod.py
+++ errander/settings/prod.py
@@ -3,16 +3,16 @@ from decouple import config
 
 SECRET_KEY = config('SECRET_KEY')
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
+EMAIL_BACKEND = config('EMAIL_BACKEND')
+EMAIL_USE_TLS = config('EMAIL_USE_TLS')
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT')
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': config('PSQL_ENGINE'),
         'NAME': config('PSQL_DB_NAME'),
         'USER': config('PSQL_USER'),
         'PASSWORD': config('PSQL_PASSWORD'),
