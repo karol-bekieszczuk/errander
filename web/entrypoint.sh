cc05624a7dac7a09fa93f7324914086e45edd84e
admin <test@test.com>
2023-05-06 19:54:43 +0200

    added venv to docker web container, updated .gitignore

--- web/entrypoint.sh
+++ web/entrypoint.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 cd web
-until python manage.py migrate
+until /opt/venv/bin/python3 manage.py migrate
 do
   echo 'migration failed, retrying'
   sleep 1
 done
-python manage.py loaddata accounts/accounts.json
-python manage.py collectstatic --no-input
-gunicorn --bind 0.0.0.0:8000 errander.wsgi
+APP_PORT=${PORT:-8000}
+/opt/venv/bin/python3 manage.py loaddata accounts/accounts.json
+/opt/venv/bin/python3 manage.py collectstatic --no-input
+#/opt/venv/bin/gunicorn --bind 0.0.0.0:8000 errander.wsgi
+/opt/venv/bin/gunicorn --worker-tmp-dir /dev/shm errander.wsgi:application --bind "0.0.0.0:${APP_PORT}"
--- web/static/errands/history.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-  $('#mapDisplayBtn').click(function(){
-    if ($('#displayTable').is(':hidden')){
-        $('#displayTable').css('display', 'block');
-    } else {
-        $('#displayTable').css('display', 'none');
-    }
-  });
-});
