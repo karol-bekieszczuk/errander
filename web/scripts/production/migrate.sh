d64c2209450a7590bba2e82644e6090ad270bc41
admin <test@test.com>
2023-05-19 15:29:15 +0200

    simplified production migrate script

--- web/scripts/production/migrate.sh
+++ web/scripts/production/migrate.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
 /opt/venv/bin/python3 manage.py makemigrations
-until /opt/venv/bin/python3 manage.py migrate --no-input
-do
-  echo 'migration failed, retrying'
-  sleep 1
-done
+/opt/venv/bin/python3 manage.py migrate --no-input
 /opt/venv/bin/python3 manage.py loaddata accounts/accounts.json
